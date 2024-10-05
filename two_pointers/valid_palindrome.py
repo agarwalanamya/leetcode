08c0439a8bd40d0b226586fc040d29e467e6b963
Anamya Agarwal <35624725+agarwalanamya@users.noreply.github.com>
2024-10-06 01:52:31 +0530

    Add solution for valid pralindrome problem

---
+++ two_pointers/valid_palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = ''.join(char for char in s if char.isalnum())
+
+        for i in range(len(s)):
+            j = len(s) - i - 1
+            if s[i] != s[j]:
+                return False
+        return True
