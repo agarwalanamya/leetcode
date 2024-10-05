c99013ce96ddbece6e6b21f6ae614d61c43eb09e
Anamya Agarwal <35624725+agarwalanamya@users.noreply.github.com>
2024-10-06 02:23:02 +0530

    Add another solution for valid_palindrome.py

--- two_pointers/valid_palindrome.py
+++ two_pointers/valid_palindrome.py
@@ -8,3 +8,12 @@ class Solution:
             if s[i] != s[j]:
                 return False
         return True
+
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = ''.join(char.lower() for char in s if char.isalnum())
+
+#         for i in range(len(s)//2):
+#             if s[i] != s[~i]:
+#                 return False
+#         return True
