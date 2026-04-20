eca060b149ef17a430fa8d2bd98d1c1b3409aadd
Anamya Agarwal <35624725+agarwalanamya@users.noreply.github.com>
2026-04-21 00:48:23 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        buy_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < buy_price:
+                buy_price = price
+            else:
+                if price - buy_price < max_profit:
+                    continue
+                else:
+                    max_profit = price - buy_price
+        
+        return max_profit
